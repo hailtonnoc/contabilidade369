f5eaf10625cfe016291f4ba1b63646b98e316911
hailtonnoc <hailton.noc@gmail.com>
2024-07-03 15:27:12 -0300

    controla carrossel

--- script.js
+++ script.js
@@ -3,20 +3,80 @@ const carousel = document.querySelector('.carousel');
 const items = document.querySelectorAll('.carousel-item');
 const totalItems = items.length;
 let currentIndex = 0;
-const delay = 8100;
+const delay = 8100; // 9 segundos
+let timeoutId;
 
 function moveCarousel() {
-    // Calcula a posição de deslocamento
     const offset = -currentIndex * 100;
     carousel.style.transform = `translateX(${offset}vw)`;
 
-    // Atualiza o índice do item atual
+    timeoutId = setTimeout(() => {
     currentIndex = (currentIndex + 1) % totalItems;
-
-    // Define o próximo movimento do carrossel
-    setTimeout(moveCarousel, delay);
+    moveCarousel();
+    }, delay);
 }
 
+function nextItem() {
+    clearTimeout(timeoutId);
+    currentIndex = (currentIndex + 1) % totalItems;
+    moveCarousel();
+}
+
+function prevItem() {
+    clearTimeout(timeoutId);
+    currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+    moveCarousel();
+}
+
+function handleTouchStart(event) {
+    touchStartX = event.touches[0].clientX;
+}
+
+function handleTouchMove(event) {
+    if (!touchStartX) {
+    return;
+    }
+
+    touchEndX = event.touches[0].clientX;
+}
+
+function handleTouchEnd() {
+    if (!touchStartX || !touchEndX) {
+    return;
+    }
+
+    const diffX = touchStartX - touchEndX;
+
+    if (diffX > 50) {
+    nextItem(); // Swipe left
+    } else if (diffX < -50) {
+    prevItem(); // Swipe right
+    }
+
+    touchStartX = null;
+    touchEndX = null;
+}
+
+// Event listeners para cliques
+document.addEventListener('click', (event) => {
+    const rect = document.body.getBoundingClientRect();
+    const clickX = event.clientX - rect.left;
+
+    if (clickX < rect.width / 2) {
+    prevItem();
+    } else {
+    nextItem();
+    }
+});
+
+// Event listeners para gestos de toque
+let touchStartX = null;
+let touchEndX = null;
+
+carousel.addEventListener('touchstart', handleTouchStart, false);
+carousel.addEventListener('touchmove', handleTouchMove, false);
+carousel.addEventListener('touchend', handleTouchEnd, false);
+
 // Inicia o carrossel
-setTimeout(moveCarousel, delay);
+moveCarousel();
 });
